1c0c5da608b876d386c8b6da255f2b7e76b42760
lisanne <lisannenederpelt@hotmail.com>
2021-04-07 15:14:22 +0200

    grid and layout changed

--- script/script.js
+++ script/script.js
@@ -34,6 +34,7 @@ map.on('load', function () {
 		// console.log(ev.result.center);
 		getAPIdata(ev.result.center[0], ev.result.center[1]);
     // getAPIdataTwee(ev.result.center[0], ev.result.center[1]);
+    getAPIdataDrie(ev.result.center[0], ev.result.center[1]);
 		});
 	});
 
@@ -53,36 +54,75 @@ map.on('load', function () {
 	// en dan gaan we iets doen met het antwoord
 		.then(function(antwoord) {
 
+      // Toon de plaatsnaam op mijn website
+     var naamlocatieBox = document.getElementById('naamPlek');
+     naamlocatieBox.innerHTML = antwoord.name;
+
 			// Toon het aantal graden (het weer) op mijn website
 			var weerBox = document.getElementById('graden');
 			weerBox.innerHTML = (antwoord.main.temp - 273.15).toFixed(1) + ' &#730;C <br>';
-      // weerBox.innerHTML = (antwoord.main.temp).toFixed(1) + ' &#730;C <br>';
 
-	    // Toon de weersbeschrijving op mijn website
-	    var beschrijvingweerBox = document.getElementById('beschrijvingweer');
-	    beschrijvingweerBox.innerHTML = antwoord.weather[0].description;
+      //kleuren bij elke temperatuur.
+      // tot 8 graden blauw
+      // vanaf 8.1 tot 14 graden oranje
+      // vanaf 14.1 graden rood
 
-  	   // Toon de plaatsnaam op mijn website
-			var naamlocatieBox = document.getElementById('naamPlek');
-			naamlocatieBox.innerHTML = antwoord.name;
-
-      var windSpeedBox = document.getElementById('handig');
-      windSpeedBox  = antwoord.clouds.all;
-        // windSpeedBox  = antwoord.wind.speed;
-
-            if (windSpeedBox < 50){
-              // windSpeedBox  = 'je kan hier landen' + 'de windsnelheid is'+ antwoord.wind.speed;
-              // console.log ('de windsnelheid is goed')
-              document.getElementById("handig").innerHTML = 'Het is ' + windSpeedBox + '%' + ' bewolkt.' + ' Het is mogelijk om hier te landen.';
-            } else if (windspeedBox => 51){
-              document.getElementById("handig").innerHTML = 'Het is ' + windSpeedBox + '%' + ' bewolkt.' + ' Het is NIET aan te raden om op deze plek te landen.';
-            }
+      if (antwoord.main.temp - 273.15.toFixed(1) < 8){
+        //blauw = het is koud
+         document.getElementById('graden').style.color = '#0D9EDE';
+      }
+      //oranje
+      else if (antwoord.main.temp - 273.15.toFixed(1) >= 8.1 && antwoord.main.temp - 273.15.toFixed(1) <= 14){
+        //oranje = het is niet te koud en niet te warm
+        // moskou
+         document.getElementById('graden').style.color = '#F4750F';
+      }
+      else if (antwoord.main.temp - 273.15.toFixed(1) >= 14.1){
+        //rood = het is warm
+        // kreta
+         document.getElementById('graden').style.color = '#E82A05';
+      }
 		});
 	}
 
 
+  //Functie waarmee ik informatie vanuit de api van WEERLIVE op de website kan zetten.
+  	function getAPIdataDrie(lon, lat) {
+      // De api die ik wil + de ingevoerde lon en lat aka coordinates (die de gebruiker zelf invoerd).
+      var  stad  =  document.getElementById('geocoder').value ;
+      	var aanvraag= 'https://weerlive.nl/api/json-data-10min.php?key=562fb057ad&locatie='  + stad;
+
+      // aanvraag om mij informatie te geven vanuit de api
+      // en doe dan iets met het antwoord van de aanvraag
+  		fetch(aanvraag).then(function(antwoord) {
+          // interpeteer het alsof het een javascript object notatie is
+  			return antwoord.json();
+  		})
+  	// en dan gaan we iets doen met het antwoord
+  		.then(function(antwoord) {
+
+        // Toon de weersbeschrijving op mijn website
+        var beschrijvingweerBox = document.getElementById('beschrijvingweer');
+        beschrijvingweerBox.innerHTML = antwoord.liveweer[0].samenv;
+
+        // variabele waarmee ik de wind in km/h weer kan geven op de website
+        var windSpeedBox = document.getElementById('handig');
+        windSpeedBox  = antwoord.liveweer[0].windkmh;
+
+              //weergeven of je hier kan landen
+              if (windSpeedBox < 50){
+                document.getElementById("handig").innerHTML = 'De windkracht is ' + windSpeedBox + ' km/h.' + ' Het is mogelijk om hier te landen.';
+              } else if (windspeedBox => 51){
+                document.getElementById("handig").innerHTML = 'De windkracht is ' + windSpeedBox + ' km/h.' + ' De wind is te hard. Het is NIET aan te raden om op deze plek te landen.';
+              }
+
+            // // Toon de weersbeschrijving op mijn website
+            // var beschrijvingweerBox = document.getElementById('kanszon');
+            // beschrijvingweerBox.innerHTML = antwoord.liveweer[0].gtemp;
 
 
+      });
+    }
 
 // popup
 	// hier komt nog een functie met het tonen of je hier wel kan landen. Onder 7 graden niet landen. boven 30 graden niet landen.
